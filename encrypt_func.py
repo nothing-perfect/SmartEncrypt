fb92b930051a0f08a9a2eb59da6e5bec945f341f
spencerrussell-dev <spencerrusselldev@gmail.com>
2023-04-29 12:16:26 -0400

    fixed problems with encryption
    next will be decryption

--- encrypt_func.py
+++ encrypt_func.py
@@ -1,36 +1,49 @@
-import cryptography
+from cryptography.fernet import Fernet
+import time
+import os
+
+#open text file in read mode
+file = open("filelocation.txt", "r")
+originalfile = file.read()
+file.close()
 
 def fernet_algo():
     #encryption algorithm 1 aka fernet
-    #key generation
+    
+    #generating the key
     key = Fernet.generate_key()
  
-    #string the key in a 
+    #writing the key to a file named 'smartencrypt.key'
     try:
-        with open('key', 'wb') as filekey:
+        with open('smartencrypt.key', 'wb') as filekey:
             filekey.write(key)
+            print ("writing key file...")
+            time.sleep(8)
             filekey.close()
-            filekey.flush
     except:
-        print ("error")
+        print ("Error creating 'smartencrypt.key'. Does SmartEncrypt have permision to write files here?")
    
     # opening the key
-    with open('key', 'rb') as filekey:
+    with open('smartencrypt.key', 'rb') as filekey:
         key = filekey.read()
  
     # using the generated key
-    fernet = Fernet(key)
+    fernetkey = Fernet(key)
  
     # opening the original file to encrypt
     with open((originalfile), 'rb') as file:
         original = file.read()
      
     # encrypting the file
-    encrypted = fernet.encrypt(original)
+    encrypted = fernetkey.encrypt(original)
  
     # opening the file in write mode and
     # writing the encrypted data
-    with open('encrypted.txt', 'wb') as encfile:
-        encfile.write(encrypted)
-        encfile.flush()
-        encfile.close()
+    try:
+        with open((originalfile), 'wb') as encfile:
+            encfile.write(encrypted)
+            print ("writing encrypted file...")
+            time.sleep(8)
+            encfile.flush()
+    except:
+        print ("error writing 'encrypted.txt'.  Does SmartEncrypt have permision to write files here?")
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #import required files/libs and setup varstorage
 import easygui
-import encrypt_func
+import encrypt_func as encryptionf
+import time
 
 #import file
 originalfile = easygui.fileopenbox()
 
+#save originalfile to be used in other scripts
+filewrite = open("filelocation.txt", "w")
+filewrite.write((originalfile))
+filewrite.close()
+
 #launch encrypt function
-encrypt_func.fernet_algo
+encryptionf.fernet_algo()
+time.sleep(13)
